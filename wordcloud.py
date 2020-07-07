d318af62e530c30780d33dcdacd30626f59a3979
Ananda Datta Sai Phanindra Tangirala <63234243+sai-phani255@users.noreply.github.com>
2020-07-07 20:11:07 +0530

    Add files via upload

---
+++ wordcloud.py
@@ -0,0 +1,27 @@
+import wikipedia
+import sys
+import numpy as np
+from PIL import Image
+from wordcloud import WordCloud  , STOPWORDS
+import cv2
+
+x=str(input('Enter the title : '))
+title=wikipedia.search(x)[0]
+page=wikipedia.page(title)
+text=page.content
+
+#print(text)
+
+background = np.array(Image.open('cloud.png'))
+stopwords = set(STOPWORDS)
+
+wc = WordCloud(background_color = 'white',max_words=244,mask=background,
+               stopwords = stopwords)
+
+wc.generate(text)
+wc.to_file('1.png')
+
+img=cv2.imread('1.png')
+cv2.imshow("Word Cloud",img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
